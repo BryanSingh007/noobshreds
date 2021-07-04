495462d52a96d58a7ee9fd2af121e61406b0a5c5
BryanSingh007 <42626790+BryanSingh007@users.noreply.github.com>
2021-07-03 23:08:27 -0700

    Add files via upload

---
+++ ex_01.cpp
@@ -0,0 +1,32 @@
+/*
+	Return Values
+
+	#include <iostream>
+	
+	int sum(int a, int b);
+
+	int main() {
+		int r = sum(10, 20);
+		std::cout << r;
+	}
+
+	int sum(int a, int b) {
+		return(a + b);
+	}
+
+
+	Parameters
+	Functions
+	Built-in Functions
+	Calling Function
+	void Functions
+	Function Declaration & Definition
+	Function Arguments
+	Scope of Code
+	Function Declarations in Header File
+	Function Template
+	Default Arguments
+	Functions Definitions
+	Function Overloading
+	Inline Functions
+*/
